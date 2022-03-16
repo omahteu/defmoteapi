ef9acc65172d37eca569d799a8d4617ca44ab182
matheus <matheus.mmn@hotmail.com>
2022-03-16 09:35:12 -0300

    ajustes

--- painel/models.py
+++ painel/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Painel(models.Model):
     objects = None
     quarto = models.CharField(max_length=2, blank=True)
+    pessoas = models.CharField(max_length=1, blank=True)
     texto = models.TextField()
     
     def __str__(self) -> str:
