785ded0550bd93994ea8f46e8c59d9e5e740fb56
matheus <matheus.mmn@hotmail.com>
2022-03-16 09:36:31 -0300

    ajustes

--- caixa/models.py
+++ caixa/models.py
@@ -6,6 +6,7 @@ class Caixa(models.Model):
     data = models.CharField(max_length=10, blank=True)
     entrada = models.CharField(max_length=10, blank=True)
     usuario = models.CharField(max_length=20, blank=True)
+    fundo = models.CharField(max_length=10, blank=True)
     total = models.CharField(max_length=10, blank=True)
     saida = models.CharField(max_length=10, blank=True)
     
